afe3f3f8df4b4bc0d2814404ed8c53e106c77799
zhuyujia <yjzhu1987@126.com>
2015-11-23 21:21:38 +0800

    格式化代码 Signed-off-by: zhuyujia <yjzhu1987@126.com>

--- utils.js
+++ utils.js
@@ -1,4 +1,4 @@
-;(function(root){
+;(function(root) {
 	'use strict';
 
 	var Utils = {
@@ -12,30 +12,30 @@
 		 * @param  {Object} opts  其他参数，有效期 expires 单位小时；路径 path，域名 domain，安全性 secure
 		 * @return {String}       有 cookie 就返回 cookie 的值，没有返回 null
 		 */
-		cookie: function(name, value, opts){
-			if(typeof value !== 'undefined'){
+		cookie: function(name, value, opts) {
+			if (typeof value !== 'undefined') {
 				var expires = '';
 				opts = opts || {};
-				if(value === null){
+				if (value === null) {
 					value = '';
 					opts.expires = -1;
 				}
-				if(opts.expires){
+				if (opts.expires) {
 					var date = new Date();
 					date.setTime(date.getTime() + (opts.expires * 60 * 60 * 1000));
 					expires = '; expires=' + date.toGMTString();
 				}
-				var path   = opts.path ? '; path=' + opts.path : '';
+				var path = opts.path ? '; path=' + opts.path : '';
 				var domain = opts.domain ? '; domain=' + opts.domain : '';
 				var secure = opts.secure ? '; secure' : '';
 				document.cookie = [name, '=', encodeURIComponent(value), expires, path, domain, secure].join('');
 			} else {
 				var cookies;
-				if(document.cookie && document.cookie !== ''){
+				if (document.cookie && document.cookie !== '') {
 					cookies = document.cookie.match(new RegExp('(^| )' + name + '=([^;]*)(;|$)'));
-					if(cookies){
+					if (cookies) {
 						return cookies[2];
-					}else{
+					} else {
 						return null;
 					}
 				}
@@ -46,74 +46,74 @@
 		 * @param {Object} obj  指代上下文，一般用 this，如：<a href="javascript:;" onclick="Utils.addFavorite(this)">点击收藏</a>
 		 * @param {Object} opts 收藏夹的标题和链接，默认为当前页面的标题和链接 {title: 'your_title', url: 'your_url'}
 		 */
-		addFavorite: function(obj, opts){
+		addFavorite: function(obj, opts) {
 			var _t, _u;
-			if(typeof opts !== 'object'){
+			if (typeof opts !== 'object') {
 				_t = document.title;
 				_u = location.href;
-			}else{
+			} else {
 				_t = opts.title || document.title;
 				_u = opts.url || location.href;
 			}
-			try{
+			try {
 				window.external.addFavorite(_u, _t);
-			}catch(e){
-				if(window.sidebar){
-				    obj.href  = _u;
-				    obj.title = _t;
-				    obj.rel   = 'sidebar';
-				}else{
-				    alert('抱歉，您所使用的浏览器无法完成此操作。\n\n请使用 Ctrl + D 将本页加入收藏夹！');
+			} catch (e) {
+				if (window.sidebar) {
+					obj.href = _u;
+					obj.title = _t;
+					obj.rel = 'sidebar';
+				} else {
+					alert('抱歉，您所使用的浏览器无法完成此操作。\n\n请使用 Ctrl + D 将本页加入收藏夹！');
 				}
 			}
 		},
 		/**
 		 * 改变文本字体大小
 		 * @param  {Object} opts 参数集合
-		 * range：			字体大小增加幅度
-		 * min：			字体最小值
-		 * max：			字体最大值
-		 * disabledClass：	禁用样式名
-		 * btnLarge：	  	增大字体按钮
-		 * btnSmall：	  	减小字体按钮
-		 * target：			字体改变的容器
+		 * range：字体大小增加幅度
+		 * min：字体最小值
+		 * max：字体最大值
+		 * disabledClass：禁用样式名
+		 * btnLarge：增大字体按钮
+		 * btnSmall：减小字体按钮
+		 * target：字体改变的容器
 		 */
-		fontSizeChange: function(opts){
-			var oTarget        = document.getElementById(opts.target),
-				oBtnLarge      = document.getElementById(opts.btnLarge),
-				oBtnSmall      = document.getElementById(opts.btnSmall),
-				oTargetStyle   = root.getComputedStyle ? root.getComputedStyle(oTarget, '') : oTarget.currentStyle,
-				iCurSize       = parseInt(oTargetStyle.fontSize, 10),
-				iMin           = opts.min || 12,
-				iMax           = opts.max || 20,
+		fontSizeChange: function(opts) {
+			var oTarget = document.getElementById(opts.target),
+				oBtnLarge = document.getElementById(opts.btnLarge),
+				oBtnSmall = document.getElementById(opts.btnSmall),
+				oTargetStyle = root.getComputedStyle ? root.getComputedStyle(oTarget, '') : oTarget.currentStyle,
+				iCurSize = parseInt(oTargetStyle.fontSize, 10),
+				iMin = opts.min || 12,
+				iMax = opts.max || 20,
 				sDisabledClass = opts.disabledClass || 'disabled',
-				regExp         = new RegExp('(\\s|^)' + sDisabledClass + '(\\s|$)');
+				regExp = new RegExp('(\\s|^)' + sDisabledClass + '(\\s|$)');
 
-			function changeSize(oBtn, num){
-				if(regExp.test(oBtn.className)){
+			function changeSize(oBtn, num) {
+				if (regExp.test(oBtn.className)) {
 					return false;
-				}else{
+				} else {
 					iCurSize += num;
 					oTarget.style.fontSize = iCurSize + 'px';
-					if(num < 0){
+					if (num < 0) {
 						oBtnLarge.className = oBtnLarge.className.replace(regExp, ' ');
-						if(iCurSize <= iMin){
+						if (iCurSize <= iMin) {
 							oBtnSmall.className += ' ' + sDisabledClass;
 						}
-					}else{
+					} else {
 						oBtnSmall.className = oBtnSmall.className.replace(regExp, ' ');
-						if(iCurSize >= iMax){
+						if (iCurSize >= iMax) {
 							oBtnLarge.className += ' ' + sDisabledClass;
 						}
 					}
 				}
 			}
 
-			oBtnLarge.onclick = function(){
+			oBtnLarge.onclick = function() {
 				changeSize(oBtnLarge, opts.num);
 			};
 
-			oBtnSmall.onclick = function(){
+			oBtnSmall.onclick = function() {
 				changeSize(oBtnSmall, -opts.num);
 			};
 		}
