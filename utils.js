c093ffd0d96e326520d502afdca0b4a0c7d9728c
zhuyujia <yjzhu1987@126.com>
2015-12-23 14:33:16 +0800

    优化 placeholder 方法 Signed-off-by: zhuyujia <yjzhu1987@126.com>

--- utils.js
+++ utils.js
@@ -9,7 +9,7 @@
 		 * 删除：Utils.cookie('name', null);
 		 * @param  {String} name  读取/写入/删除 cookie 的名称
 		 * @param  {String} value 需要设置 cookie 的值
-		 * @param  {Object} opts  其他参数，有效期 expires 单位小时；路径 path，域名 domain，安全性 secure
+		 * @param  {Object} opts  其他参数，有效期 expires 单位小时，路径 path，域名 domain，安全性 secure
 		 * @return {String}       有 cookie 就返回 cookie 的值，没有返回 null
 		 */
 		cookie: function(name, value, opts) {
@@ -42,8 +42,8 @@
 			}
 		},
 		/**
-		 * 添加到收藏夹，使用方法：Utils.addFavorite(obj, {'title': 'your_title', 'url': 'your_url'});
-		 * @param {Object} obj  指代触发函数的上下文，一般用 this，如：<a href="javascript:;" onclick="Utils.addFavorite(this)">点击收藏</a>
+		 * 添加到收藏夹，使用方法：<a href="javascript:;" onclick="Utils.addFavorite(this)">点击收藏</a>
+		 * @param {Object} obj  指代触发函数的上下文，一般用 this
 		 * @param {Object} opts 收藏夹的标题和链接，默认为当前页面的标题和链接 {title: 'your_title', url: 'your_url'}
 		 */
 		addFavorite: function(obj, opts) {
@@ -118,31 +118,40 @@
 			};
 		},
 		/**
-		 * 输入框占位符提示语，支持 input 和 textarea 标签，使用方法：Utils.placeholder('name', '请输入', 'tip'); 由于 placeholder 在 ie 浏览器中效果不理想，比如 ie6-9 不支持 placeholder，ie10 鼠标聚焦后文本消失，所以在 ie 浏览器中使用 label 标签模拟。
+		 * 输入框占位符提示语，支持 input 和 textarea 标签，使用方法：Utils.placeholder('name', '请输入');
+		 * 由于 placeholder 在 ie 浏览器中效果不理想，比如 ie6-9 不支持 placeholder，ie10 鼠标聚焦后文本消失，
+		 * 所以在 ie 浏览器中使用 label 标签模拟。
 		 * @param  {String} id        文本输入框 id
 		 * @param  {String} msg       占位符提示语文字
-		 * @param  {String} className ie 浏览器占位符样式，默认 tip-id，比如 id 为 name，那么样式名称为 'tip-name'
+		 * @param  {String} className ie 浏览器占位符样式，默认 placeholder-tip
 		 */
 		placeholder: function(id, msg, className) {
 			var isPlaceholder = 'placeholder' in document.createElement('input'),
 				isIE = navigator.userAgent.indexOf('Trident') >= 0 ? true : false,
 				oTarget = document.getElementById(id),
 				oParent = oTarget.parentNode,
-				oLabel = document.createElement('label');
+				oLabel = document.createElement('label'),
+				className = className || 'placeholder-tip',
+				isSupportSetAttr = true;
 
 			if (isPlaceholder && !isIE) {
 				oTarget.setAttribute('placeholder', msg);
 			} else {
-				oLabel.setAttribute('for', id);
-				oLabel.setAttribute('class', className || 'tip-' + id);
+				oLabel.setAttribute('class', className);
+				//在 ie6/7 中，setAttribute 设置 class 和 for 属性无效，需要设置 className 和 htmlFor 属性有效，所以此时 isSupportSetAttr 为 false，当然可以通过 oLabel.className 来设置 class，但是该方法对于 for 属性没用
+				isSupportSetAttr = oLabel.className === className;
+				//在 ie6/7 中，由于之前通过 setAttribute 已经设置过了 class 属性，后面设置 className 属性会在页面中重复 class 属性，虽然没有什么影响，但是以防万一还是先把之前设置的 class 属性删除
+				if(!isSupportSetAttr){
+					oLabel.removeAttribute('class');
+					oLabel.setAttribute('className', className);
+				}
+				oLabel.setAttribute(isSupportSetAttr ? 'for' : 'htmlFor', id);
 				oLabel.innerHTML = msg;
 				oParent.insertBefore(oLabel, oTarget);
-				if (oTarget.value !== '') {
-					oLabel.style.display = 'none';
-				}
-				oTarget.oninput = oTarget.onpropertychange = function() {
+				oLabel.style.display = oTarget.value ? 'none' : 'block';
+				oTarget.oninput = oTarget.onpropertychange = oTarget.onkeydown = oTarget.onkeyup = function(){
 					oLabel.style.display = oTarget.value ? 'none' : 'block';
-				};
+				}
 			}
 		},
 		/**
